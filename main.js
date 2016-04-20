011e348512d5aec22de54e51c2fab83eb50af94a
Rafael Jaques <rafael@phpit.com.br>
2016-04-20 20:22:14 -0300

    Some minor bugfixes

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const Configstore = require('configstore');
 const pkg = require(Path.join(__dirname, 'package.json'));
 const conf = new Configstore(pkg.name);
 
-// Icon and window
+// Icon, window and app name
+const appName = "PHP Assistant"
 var appIcon = null;
 var mainWindow = null;
 
@@ -26,6 +27,7 @@ app.on('ready', function() {
 
   // Creates window
   mainWindow = new BrowserWindow({
+      "title": appName,
       "height": 600,
       "width": 900,
       "icon": Path.join(__dirname, "gfx", "app-icon.png"),
@@ -73,7 +75,7 @@ function createTrayIcon() {
 
   appIcon = new Tray(trayIcon);
 
-  appIcon.setToolTip("PHP Assistant");
+  appIcon.setToolTip(appName);
   appIcon.on("click", focusBehavior);
 }
 
@@ -121,203 +123,220 @@ function runOnApp(func) {
 
 // Creates app menu
 function createMenu() {
-    var template = [
-    {
-      label: i18n.__('Actions'),
-      submenu: [
-        {
-          label: i18n.__('Run code'),
-          accelerator: 'CmdOrCtrl+Return',
-          click: function() { runOnApp('runCode()'); }
-        },
-        {
-          label: i18n.__('Clear'),
-          accelerator: 'CmdOrCtrl+Shift+C',
-          click: function() { runOnApp('clear()'); }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Import from file'),
-          accelerator: 'CmdOrCtrl+I',
-          click: function() { runOnApp('importFromFile()'); }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Toggle mode'),
-          accelerator: 'CmdOrCtrl+T',
-          click: function() { runOnApp('toggleMode()'); }
-        },
-      ]
-    },
-    {
-      label: i18n.__('Edit'),
-      submenu: [
-        {
-          label: i18n.__('Undo'),
-          accelerator: 'CmdOrCtrl+Z',
-          role: 'undo'
-        },
-        {
-          label: i18n.__('Redo'),
-          accelerator: 'Shift+CmdOrCtrl+Z',
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Cut'),
-          accelerator: 'CmdOrCtrl+X',
-          role: 'cut'
-        },
-        {
-          label: i18n.__('Copy'),
-          accelerator: 'CmdOrCtrl+C',
-          role: 'copy'
-        },
-        {
-          label: i18n.__('Paste'),
-          accelerator: 'CmdOrCtrl+V',
-          role: 'paste'
-        },
-        {
-          label: i18n.__('Select All'),
-          accelerator: 'CmdOrCtrl+A',
-          role: 'selectall'
-        },
-      ]
-    },
-    {
-      label: i18n.__('View'),
-      submenu: [
-        {
-          label: i18n.__('Toggle Full Screen'),
-          accelerator: (function() {
-            if (process.platform == 'darwin')
-              return 'Ctrl+Command+F';
-            else
-              return 'F11';
-          })(),
-          click: function(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-          }
-        },
-      ]
-    },
-    {
-      label: i18n.__('Window'),
-      role: 'window',
-      submenu: [
-        {
-          label: i18n.__('Minimize'),
-          accelerator: 'CmdOrCtrl+M',
-          role: 'minimize'
-        },
-      ]
-    },
-    {
-      label: i18n.__('Help'),
-      role: 'help',
-      submenu: [
-        {
-          label: i18n.__('PHP Assistant on GitHub'),
-          click: function() { require('electron').shell.openExternal('https://github.com/rafajaques/php-assistant') }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Submit an issue'),
-          click: function() { require('electron').shell.openExternal('https://github.com/rafajaques/php-assistant/issues') }
-        },
-      ]
-    },
-  ];
-
-  // OSX specific menu
-  if (process.platform == 'darwin') {
-    var name = "PHP Assistant"
-    template.unshift({
-      label: name,
-      submenu: [
-        {
-          label: i18n.__('About') + ' ' + name,
-          click: function() { runOnApp("$('#about').modal('toggle');"); }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Preferences') + '...',
-          accelerator: 'Cmd+,',
-          click: function() { runOnApp("$('#settings').modal('show');"); }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Services'),
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Hide') + ' ' + name,
-          accelerator: 'Command+H',
-          role: 'hide'
-        },
-        {
-          label: i18n.__('Hide Others'),
-          accelerator: 'Command+Alt+H',
-          role: 'hideothers'
-        },
-        {
-          label: i18n.__('Show All'),
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: i18n.__('Quit'),
-          accelerator: 'Command+Q',
-          click: function() { app.quit(); }
-        },
-      ]
-    });
-    // Window menu.
-    template[3].submenu.push(
+  var template = [
+  {
+    label: i18n.__('Actions'),
+    submenu: [
+      {
+        label: i18n.__('Run code'),
+        accelerator: 'CmdOrCtrl+Return',
+        click: function() { runOnApp('runCode()'); }
+      },
+      {
+        label: i18n.__('Clear'),
+        accelerator: 'CmdOrCtrl+Shift+C',
+        click: function() { runOnApp('clear()'); }
+      },
       {
         type: 'separator'
       },
       {
-        label: i18n.__('Bring All to Front'),
-        role: 'front'
-      }
-    );
-  }
-
-  // If this is not OSX, set Preferences menu somewhere else...
-  else {
-    template[0].submenu.push(
+        label: i18n.__('Import from file'),
+        accelerator: 'CmdOrCtrl+I',
+        click: function() { runOnApp('importFromFile()'); }
+      },
       {
         type: 'separator'
       },
       {
-        label: i18n.__('Preferences'),
-        accelerator: 'Ctrl+P',
+        label: i18n.__('Toggle mode'),
+        accelerator: 'CmdOrCtrl+T',
+        click: function() { runOnApp('toggleMode()'); }
+      },
+    ]
+  },
+  {
+    label: i18n.__('Edit'),
+    submenu: [
+      {
+        label: i18n.__('Undo'),
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: i18n.__('Redo'),
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Cut'),
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: i18n.__('Copy'),
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: i18n.__('Paste'),
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: i18n.__('Select All'),
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      },
+    ]
+  },
+  {
+    label: i18n.__('View'),
+    submenu: [
+      {
+        label: i18n.__('Toggle Full Screen'),
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+    ]
+  },
+  {
+    label: i18n.__('Window'),
+    role: 'window',
+    submenu: [
+      {
+        label: i18n.__('Minimize'),
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+    ]
+  },
+  {
+    label: i18n.__('Help'),
+    role: 'help',
+    submenu: [
+      {
+        label: appName + ' ' + i18n.__('on GitHub'),
+        click: function() { require('electron').shell.openExternal('https://github.com/rafajaques/php-assistant') }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Submit an issue'),
+        click: function() { require('electron').shell.openExternal('https://github.com/rafajaques/php-assistant/issues') }
+      },
+    ]
+  },
+];
+
+// OSX specific menu
+if (process.platform == 'darwin') {
+  template.unshift({
+    label: appName,
+    submenu: [
+      {
+        label: i18n.__('About') + ' ' + appName,
+        click: function() { runOnApp("$('#about').modal('toggle');"); }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Preferences') + '...',
+        accelerator: 'Cmd+,',
         click: function() { runOnApp("$('#settings').modal('show');"); }
-      }
-    );
-  }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Services'),
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Hide') + ' ' + appName,
+        accelerator: 'Command+H',
+        role: 'hide'
+      },
+      {
+        label: i18n.__('Hide Others'),
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers'
+      },
+      {
+        label: i18n.__('Show All'),
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.__('Quit'),
+        accelerator: 'Command+Q',
+        click: function() { app.quit(); }
+      },
+    ]
+  });
+  // Window menu.
+  template[3].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: i18n.__('Bring All to Front'),
+      role: 'front'
+    }
+  );
+}
 
-  var menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+// If this is not OSX, set missing menus somewhere else...
+else {
+  template[0].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: i18n.__('Preferences'),
+      accelerator: 'Ctrl+P',
+      click: function() { runOnApp("$('#settings').modal('show');"); }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: i18n.__('Quit'),
+      accelerator: 'Ctrl+Q',
+      click: function() { app.quit(); }
+    }
+  );
+
+  template[4].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: i18n.__('About') + ' ' + appName,
+      click: function() { runOnApp("$('#about').modal('toggle');"); }
+    }
+  );
+}
+
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 }
