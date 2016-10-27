7086618273728008fcac9909724f3cafbd262766
Will Vincent <will.vincent@ffwagency.com>
2016-10-27 00:38:00 -0500

    Fix eslint errors

--- app/js/main.functions.js
+++ app/js/main.functions.js
@@ -229,21 +229,19 @@ function populateThemes(where) {
  */
 function renderApp(refresh) {
   document.ondragover = document.ondrop = (ev) => {
-    ev.preventDefault()
-  }
+    ev.preventDefault();
+  };
 
   document.body.ondrop = (ev) => {
-    setOutput('');
-    var extensions = ['php', 'phtml', 'tpl', 'ctp'];
-    var fileparts = ev.dataTransfer.files[0].path.split('.');
+    const extensions = ['php', 'phtml', 'tpl', 'ctp'];
+    const fileparts = ev.dataTransfer.files[0].path.split('.');
     if (extensions.indexOf(fileparts[fileparts.length - 1]) >= 0) {
       fs.readFile(ev.dataTransfer.files[0].path, 'utf8', importReady);
-    }
-    else {
+    } else {
       setOutput(i18n.__('Drag & Drop file loading is only allowed for php files with one of the following extensions: .php, .phtml, .tpl, .ctp'));
     }
-    ev.preventDefault()
-  }
+    ev.preventDefault();
+  };
   // Render editor
   editor.setTheme('ace/theme/' + conf.get('editor.theme')); // This is not a path
   editor.setShowPrintMargin(false);
