9ab81352b152a108fd3416a43a4f41c8023d14fe
Will Vincent <will.vincent@ffwagency.com>
2016-10-28 03:15:36 -0500

    Fix language selection

--- app/js/welcome.js
+++ app/js/welcome.js
@@ -33,7 +33,7 @@ $.each(i18n.fullLocaleList, (i, v) => {
 });
 
 // Try to figure out user's language
-$('#locales-list').val(app.getLocale() ? app.getLocale() : 'en');
+$('#locales-list').val(app.getLocale() ? app.getLocale().split('-')[0] : 'en');
 
 /* Go button action */
 $('#go').click(() => {
